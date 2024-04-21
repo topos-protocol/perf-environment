441f3bd7fa666a4b075e9c7b3c246a4239f50b55
Bastian Gruber <foreach@me.com>
2024-04-21 18:01:06 -0300

    fix: delete data, update script

--- perf.sh
+++ perf.sh
@@ -3,20 +3,16 @@
 # Start the docker-compose services
 docker compose up -d
 
-# Wait for the application to start properly
-echo "Waiting for the network to start..."
-sleep 30 
-
 # Find the PID of the running topos application
 PID=$(docker exec topos-node-1 pgrep topos)
 
 # Start perf monitoring
 echo "Starting perf monitoring..."
-docker exec -it topos-node-1 perf record -p $PID -o /data/perf.data sleep 300
+docker exec -it topos-node-1 perf record -F 99 -g -p $PID -o /data/perf.data sleep 300
 
 # Generate a report
-echo "Generating perf report..."
-docker exec -it topos-node-1 perf report -i /data/perf.data > ./perf_outputs/perf.report
+echo "Saving perf.data..."
+docker exec -it topos-node-1 perf -i /data/perf.data > ./perf_outputs/out.perf
 
 # Stop the container
 docker-compose down
