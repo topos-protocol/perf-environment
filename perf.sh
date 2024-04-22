d442ac098696a89a05de5978aad6f88116316ce5
Bastian Gruber <foreach@me.com>
2024-04-21 21:34:00 -0300

    fix: cleanup

--- perf.sh
+++ perf.sh
@@ -12,10 +12,10 @@ docker exec -it topos-node-1 perf record -F 99 -g -p $PID -o /data/perf.data sle
 
 # Generate a report
 echo "Saving perf.data..."
-docker exec -it topos-node-1 perf -i /data/perf.data > ./perf_outputs/out.perf
+docker exec -it topos-node-1 perf script -i /data/perf.data > ./perf_outputs/out.perf
 
 # Stop the container
-docker-compose down
+docker compose down -v
 
-echo "Monitoring complete. Data saved to ./perf_outputs/perf.report"
+echo "Monitoring complete. Data saved to ./perf_outputs/out.perf"
 
